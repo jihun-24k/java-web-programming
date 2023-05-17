1c14afa6911c0f028564a7b2695f86b95276fbd1
jihun-24k <gi020477@gmail.com>
2023-05-17 16:19:35 +0900

    feat: requestLine null 처리

--- wep-application-server/src/main/java/util/URLUtils.java
+++ wep-application-server/src/main/java/util/URLUtils.java
@@ -3,6 +3,9 @@ package util;
 public class URLUtils {
 
     public static String getURL(String requestLine) {
+        if (requestLine == null) {
+            return "/";
+        }
         String[] info = requestLine.split(" ");
         return info[1];
     }
