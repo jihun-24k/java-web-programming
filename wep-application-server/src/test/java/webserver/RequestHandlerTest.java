5073e0b8f306698ba6db796a8f5ce1508f429b79
jihun-24k <gi020477@gmail.com>
2023-05-19 16:57:31 +0900

    test: get 방식으로 회원가입 테스트 코드

--- wep-application-server/src/test/java/webserver/RequestHandlerTest.java
+++ wep-application-server/src/test/java/webserver/RequestHandlerTest.java
@@ -2,7 +2,9 @@ package webserver;
 
 import static org.junit.Assert.assertEquals;
 
+import db.DataBase;
 import java.io.IOException;
+import model.User;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -12,8 +14,20 @@ public class RequestHandlerTest {
 
     @Test
     public void response_Default() throws IOException {
-        HttpGet request = new HttpGet("http://localhost:8080/");
+        HttpGet request = new HttpGet("http://localhost:8080/index.html");
         HttpResponse response = HttpClientBuilder.create().build().execute(request);
         assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    @Test
+    public void login_GET_Method() throws IOException {
+        HttpGet request = new HttpGet("http://localhost:8080/create?userId=test&password=1234&name=gildong&email=test@example.com");
+        HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+        User findUser = DataBase.findUserById("test");
+
+        assertEquals("1234", findUser.getPassword());
+        assertEquals("gildong", findUser.getName());
+        assertEquals("test@example.com", findUser.getEmail());
     }
 }
