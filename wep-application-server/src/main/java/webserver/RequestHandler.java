82010de6adc9f585e9933c429335b9c94fe6e2e2
jihun-24k <gi020477@gmail.com>
2023-08-03 11:33:34 +0900

    feat: 클라이언트 헤더에 세션 아이디 반환

--- wep-application-server/src/main/java/webserver/RequestHandler.java
+++ wep-application-server/src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Random;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import controller.Controller;
@@ -27,6 +29,10 @@ public class RequestHandler extends Thread {
 
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
+
+            if (request.getCookie("JSESSIONID") == null) {
+                response.addHeader("Set-Cookie", "JSESSIONID" + ": "+ UUID.randomUUID());
+            }
 
             Controller controller = RequestMapping.getControllers(request.getPath());
             if (controller == null) {
