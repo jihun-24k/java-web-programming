6e97f0e7c16241e2606e5a4625579d7830b2aa45
jihun-24k <gi020477@gmail.com>
2023-08-07 14:21:34 +0900

    feat: 클라이언트별 세션 추가

--- wep-application-server/src/main/java/webserver/RequestHandler.java
+++ wep-application-server/src/main/java/webserver/RequestHandler.java
@@ -30,7 +30,7 @@ public class RequestHandler extends Thread {
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
 
-            if (request.getCookies().getCookie("JSESSIONID") == null) {
+            if (request.getSession() == null) {
                 response.addHeader("Set-Cookie", "JSESSIONID" + ": "+ UUID.randomUUID());
             }
 
